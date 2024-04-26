1733c8c8428d2daaec6ab9e46eee248ab7b82f5b
borisGansGit <bgans.ieu2023@student.ie.edu>
2024-04-26 20:32:02 +0200

    Working markers and tweaked menu; zoom button top right is gone?

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,53 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 // import Leaflet from ""
 import "./map.js";
-import CSS from "./style.css";
+import "./style.css";
 // import Leaflet from "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
 
 console.log("Index.js");
+
+const nav = ReactDOM.createRoot(document.getElementById('mySidenav'));
+const menuButton = ReactDOM.createRoot(document.getElementById('button'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function openNav() {
+    // nav.style = {{"--base": width: "150px"}};
+    nav.render(
+      <div>
+        <a onClick={closeNav} id="closebtn">&times;</a>
+        <a href="#">Save Map</a>
+        <a href="homePage.html">Library</a>
+        <a href="#">Groups</a>
+        <a href="#"></a>
+      </div>
+    )
+    menuButton.render(
+      <div></div>
+    )
+    // document.getElementById("button").style.visibility = "hidden";
+}
+  
+  /* Set the width of the side navigation to 0 */
+function closeNav() {
+    nav.render(
+      <div></div>
+    )
+    menuButton.render(
+      <button onClick={openNav}>
+        <img id="img" src="badMenu.png"/>
+      </button>
+    )
+    // nav.style.width = "0";
+    // document.getElementById("button").style.visibility = "visible";
+}
+
+menuButton.render(
+  <button onClick={openNav}>
+    <img id="img" src="badMenu.png"/>
+
+  </button>
+)
+
 root.render(
   <React.StrictMode>
     <App />
--- frontend/src/map.js
+++ frontend/src/map.js
@@ -1,104 +1,91 @@
-import L from "leaflet";
-import { Map, TileLayer, Marker, Popup, MapContainer } from 'react-leaflet';
-import React, {Component} from "react";
+import L, { Control, Icon } from "leaflet";
+import { Map, TileLayer, Marker, Popup, MapContainer} from 'react-leaflet';
+import * as rl from "react-leaflet";
+import React, {useState} from "react";
 import ReactDOM from 'react-dom/client';
 
-
-
-
-
 console.log("Map");
+
+
+const map = ReactDOM.createRoot(document.getElementById('map'));
 
 
 var hardCorner1 = L.latLng(40.855141, -3.846709);
 var hardCorner2 = L.latLng(41.037635, -4.295787);
 var bounds = L.latLngBounds(hardCorner1, hardCorner2);
 
-var animationCorner1 = L.latLng(40.992416, -4.243838);
-var animationCorner2 = L.latLng(40.893644, -4.021705);
-var animationBounds = L.latLngBounds(animationCorner1, animationCorner2);
+// var animationCorner1 = L.latLng(40.992416, -4.243838);
+// var animationCorner2 = L.latLng(40.893644, -4.021705);
+// var animationBounds = L.latLngBounds(animationCorner1, animationCorner2);
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "150px";
-    document.getElementById("button").style.visibility = "hidden";
+var selectedPosition = [];
 
-}
-  
-  /* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("button").style.visibility = "visible";
-}
+const xIcon = new Icon({
+    iconUrl: "x.png",
+    iconSize: [35, 35]
+})
 
-const map = ReactDOM.createRoot(document.getElementById('map'));
+// const Markers = () => {
+//     const map = rl.useMapEvents({ 
+//         click(e) {
+//             selectedPosition = [
+//                 e.latlng.lat,
+//                 e.latlng.lng
+//             ];
+//         },
+//     })
+//     return (
+//         selectedPosition ?
+//             <Marker 
+//                 key={selectedPosition[0]}
+//                 position={selectedPosition}
+//                 icon={xIcon}
 
-function MyMap() {
+            
+//             />
+//         : null
+//     )
+// }
+
+
+const MyMap = () => {
+    const [selectedPosition, setSelectedPosition] = useState(null);
+    const [markers, setMarkers] = useState([]);
+
+    const handleMapClick = (e) => {
+        const newPosition = [e.latlng.lat, e.latlng.lng];
+        console.log(newPosition);
+        setSelectedPosition(newPosition);
+        setMarkers([...markers, newPosition]);
+    };
+
     return (
-        <MapContainer center={[40.942902, -4.1088]} zoom={13} zoomControl={false} style={{width: "100%", height:"100%"}}>
+        <MapContainer center={[40.942902, -4.1088]} zoom={13} zoomControl={false} style={{width: "100%", height:"100%"}} maxBounds={bounds} minZoom={12}>
             <TileLayer 
                 attribution='&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>'
                 url="https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey={apikey}"
                 apikey="bac66d3ee926413f8dcd9fdbdac07442"
                 maxZoom={22}
             />
+            {markers.map((position, index) => (
+                <Marker key={index} position={position} />
+            ))}
+            <MapClickHandler onClick={handleMapClick} />
+            
+            {/* <Icon url="x.png" size={[35, 35]}/> */}
+            {/* <Marker></Marker> */}
         </MapContainer>
-
+        
     )
 }
-map.render(MyMap())
+const MapClickHandler = ({ onClick }) => {
+    rl.useMapEvents({
+        click(e) {
+            onClick(e);
+        },
+    });
+    return null;
+};
 
-
-// export default MyMap()
- 
-
-// const MapExport = () => {
-    // const map = new L.Map('map', {
-    //     center: [40.942902, -4.1088],
-    //     // center: [39, 34],
-    //     zoom: 13,
-    //     minZoom: 13,
-    //     zoomControl: false
-    // });
-    
-    // L.tileLayer('https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey={apikey}', {
-    //     attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    //     apikey: 'bac66d3ee926413f8dcd9fdbdac07442',
-    //     maxZoom: 22
-    // }).addTo(map);
-    
-    // var myIcon = new L.icon({
-    //     iconUrl: "x.png",
-    //     iconSize: [35, 35],
-    //     // iconAnchor: [22, 94],
-    //     // popupAnchor: [-3, -76],
-    //     // shadowUrl: 'my-icon-shadow.png',
-    //     // shadowSize: [68, 95],
-    //     // shadowAnchor: [22, 94]
-    // })
-    
-    // L.Marker.prototype.options.icon = myIcon;
-    
-    
-    // L.control.zoom({
-    //     position: 'topright'
-    // }).addTo(map);
-    
-    // map.setMaxBounds(bounds);
-    
-    // map.on("click", function(e) {
-    //     var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-    // })
-    // return (
-    //     Map
-    //     // console.log(data)
-    // )
-// }
-
-
-// map.on("click", onMapClick(e));
-// function onMapClick(e) {
-//     var marker = L.marker(e.latLng).addTo(map);
-// }
-// export default map
-
-
+map.render(<MyMap/>)
+// ReactDOM.render(<MyMap />, document.getElementById('map'));
