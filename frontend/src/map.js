a778fb751470c21ee1a631f0bfde5643080371ec
borisGansGit <bgans.ieu2023@student.ie.edu>
2024-04-26 17:16:05 +0200

    Before discovering react-leaflet; broken cause I can't return an object in reactJS DOM

--- frontend/src/map.js
+++ frontend/src/map.js
@@ -1,15 +1,20 @@
 import L from "leaflet";
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, {Component} from "react";
+
+
+
 
 console.log("Map");
 
 
-var hardCorner1 = new L.latLng(40.855141, -3.846709);
-var hardCorner2 = new L.latLng(41.037635, -4.295787);
-var bounds = new L.latLngBounds(hardCorner1, hardCorner2);
+var hardCorner1 = L.latLng(40.855141, -3.846709);
+var hardCorner2 = L.latLng(41.037635, -4.295787);
+var bounds = L.latLngBounds(hardCorner1, hardCorner2);
 
-var animationCorner1 = new L.latLng(40.992416, -4.243838);
-var animationCorner2 = new L.latLng(40.893644, -4.021705);
-var animationBounds = new L.latLngBounds(animationCorner1, animationCorner2);
+var animationCorner1 = L.latLng(40.992416, -4.243838);
+var animationCorner2 = L.latLng(40.893644, -4.021705);
+var animationBounds = L.latLngBounds(animationCorner1, animationCorner2);
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "150px";
@@ -23,46 +28,96 @@ function closeNav() {
     document.getElementById("button").style.visibility = "visible";
 }
 
+export default class Map extends Component {
+    state = {
+        lat: 37.7749,
+        lng: -122.4194,
+        zoom: 13,
+    }
+    render() {
+        return (
+            this.props.incidents ?
+            <Map 
+               center={[this.state.lat, this.state.lng]} 
+               zoom={this.state.zoom} 
+               style={{ width: '100%', height: '900px'}}>
+            <TileLayer
+              attribution='&copy <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {
+            this.props.incidents.map(incident => {
+                const point = [incident['point']['coordinates'][1],incident['point']['coordinates'][0]]
+         
 
-var map = new L.Map('map', {
-    center: [40.942902, -4.1088],
-    // center: [39, 34],
-    zoom: 13,
-    minZoom: 13,
-    zoomControl: false
-});
+            return (
+                <Marker position={point} key={incident['incident_number']} >
+                    <Popup>
+                        <span>ADDRESS: {incident['address']}, {incident['city']} - {incident['zip_code']}</span>
+                        <br/>
+                        <span>BATTALION: {incident['battalion']}</span><br/>
+                    </Popup>
+                </Marker>
+            )
+            })
+            }
+           </Map>
+            :
+            "Map is loading"
 
-L.tileLayer('https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey={apikey}', {
-	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	apikey: 'bac66d3ee926413f8dcd9fdbdac07442',
-	maxZoom: 22
-}).addTo(map);
+        )
+     }
+ }
+ 
 
-var myIcon = new L.icon({
-    iconUrl: "x.png",
-    iconSize: [35, 35],
-    // iconAnchor: [22, 94],
-    // popupAnchor: [-3, -76],
-    // shadowUrl: 'my-icon-shadow.png',
-    // shadowSize: [68, 95],
-    // shadowAnchor: [22, 94]
-})
+// const MapExport = () => {
+    const map = new L.Map('map', {
+        center: [40.942902, -4.1088],
+        // center: [39, 34],
+        zoom: 13,
+        minZoom: 13,
+        zoomControl: false
+    });
+    
+    L.tileLayer('https://{s}.tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey={apikey}', {
+        attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        apikey: 'bac66d3ee926413f8dcd9fdbdac07442',
+        maxZoom: 22
+    }).addTo(map);
+    
+    var myIcon = new L.icon({
+        iconUrl: "x.png",
+        iconSize: [35, 35],
+        // iconAnchor: [22, 94],
+        // popupAnchor: [-3, -76],
+        // shadowUrl: 'my-icon-shadow.png',
+        // shadowSize: [68, 95],
+        // shadowAnchor: [22, 94]
+    })
+    
+    L.Marker.prototype.options.icon = myIcon;
+    
+    
+    L.control.zoom({
+        position: 'topright'
+    }).addTo(map);
+    
+    map.setMaxBounds(bounds);
+    
+    map.on("click", function(e) {
+        var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+    })
+    // return (
+    //     Map
+    //     // console.log(data)
+    // )
+// }
 
-L.Marker.prototype.options.icon = myIcon;
-
-
-L.control.zoom({
-    position: 'topright'
-}).addTo(map);
-
-map.setMaxBounds(bounds);
-
-map.on("click", function(e) {
-    var marker = new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-})
 
 // map.on("click", onMapClick(e));
 // function onMapClick(e) {
 //     var marker = L.marker(e.latLng).addTo(map);
 // }
-export default Map
+// export default map
+
+
